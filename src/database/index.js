dc4e9dae01109abb25fabad0d61ea0f197509080
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-03-30 14:24:18 -0300

    Agendamento de serviços

--- src/database/index.js
+++ src/database/index.js
@@ -2,11 +2,12 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
 // Arrays de Models da aplicação
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
@@ -17,7 +18,9 @@ class Database {
     this.connection = new Sequelize(databaseConfig);
 
     models
+      // Faz a conexão com o banco
       .map((model) => model.init(this.connection))
+      // Faz os relacionamentos entre tabelas/models
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
