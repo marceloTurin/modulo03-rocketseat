ea6c51135ac96c8dcd2a02c781a5f5b155bad127
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-03-30 16:35:55 -0300

    Agendamento de serviços

--- src/database/migrations/20200330193010-create-appointments.js
+++ src/database/migrations/20200330193010-create-appointments.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('files', {
+    return queryInterface.createTable('appointments', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -40,6 +40,6 @@ module.exports = {
   },
 
   down: (queryInterface) => {
-    return queryInterface.dropTable('files');
+    return queryInterface.dropTable('appointments');
   },
 };
