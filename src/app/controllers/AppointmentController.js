bb4797bb975d2bde0f294bfacfa914a326a31344
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-03-30 17:32:02 -0300

    Aplicando paginação

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,8 +2,35 @@ import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      // Referencia os dados do provedor do agendamento
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          // Referencia os dados do avatar do provedor
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
