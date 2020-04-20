63e40f66780b0705100a32e810a1af3010a65ea7
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-04-20 00:44:01 -0300

    Ajustes na API

--- src/app/controllers/SessionControler.js
+++ src/app/controllers/SessionControler.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 
 import User from '../models/User';
+import File from '../models/File';
 import authConfig from '../../config/auth';
 
 class SessionControler {
@@ -17,7 +18,16 @@ class SessionControler {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     // Verifica se o usuário existe
     if (!user) {
@@ -28,13 +38,15 @@ class SessionControler {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        provider,
+        avatar,
       },
       // Configuração do JTW
       token: jwt.sign({ id }, authConfig.secret, {
